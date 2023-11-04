c688bb9393549a82f2a34bb1746af9e2ea78eedf
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2023-11-03 22:15:13 -0300

    feature: search movie by name

--- src/api/routes/movies/index.js
+++ src/api/routes/movies/index.js
@@ -1,8 +1,9 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
-const { moviesController } = require('../../controllers');
+const { moviesController } = require("../../controllers");
 
-router.get('/get-movies-list', moviesController.getMoviesList);
-router.get('/get-movie/:id', moviesController.getMovie);
+router.get("/get-movies-list", moviesController.getMoviesList);
+router.get("/get-movie/:id", moviesController.getMovie);
+router.get("/get-search-movie/:name", moviesController.getSearchMovie);
 
 module.exports = router;
