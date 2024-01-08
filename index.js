d055866a21f0bafbc7a8c1aa04491c7f45d0451f
rxvinicius <vinicius-rodrigues2000@hotmail.com>
2024-01-08 18:03:38 -0300

    tests

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { moviesRouter } = require("./api/routes");
+const { moviesRouter } = require("./src/api/routes");
 const port = process.env.PORT || 3000;
 
 const startServer = async () => {
